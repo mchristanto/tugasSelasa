7efec07e9f557eab64c6be6e9a3c49c3ca6dd05c
michelltc <mchristanto01@student.ciputra.ac.id>
2021-12-10 09:16:12 +0700

    Initial check-in of module cb

---
+++ cb/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace cb
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("INPUT KALIMAT   :");
+            var kalimat = Console.ReadLine();
+            if (kalimat.Length <= 3)
+            {
+                Console.WriteLine("KATA PADA KALIMAT KURANG PANJANG");
+                    
+            }
+            Console.Write("INPUT KATA      :");
+            var kata = Console.ReadLine();
+            if (kata.Length <= 1)
+            {
+                Console.WriteLine("HURUF PADA KATA KURANG PANJANG");
+            }
+            Console.WriteLine("OUTPUT          :");
+            Console.Write("STATISTIK HURUF :");
+           
+        }
+    }
+}
